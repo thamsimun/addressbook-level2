21e7b3604d6223093a1fa97fbccedeb68efce11a
thamsimun <e0201281@u.nus.edu>
2019-02-11 15:50:09 +0800

    Add print command and edit test cases

--- src/seedu/addressbook/commands/PrintCommand.java
+++ src/seedu/addressbook/commands/PrintCommand.java
@@ -1,4 +1,13 @@
 package seedu.addressbook.commands;
 
-public class PrintCommand {
+public class PrintCommand extends Command{
+
+    public static final String COMMAND_WORD = "print";
+
+    public static final String MESSAGE_PRINT = "Hi, this is address book level 2.";
+
+    @Override
+    public CommandResult execute() {
+        return new CommandResult(MESSAGE_PRINT);
+    }
 }
--- test/java/seedu/addressbook/commands/PrintCommandTest.java
+++ test/java/seedu/addressbook/commands/PrintCommandTest.java
@@ -1,4 +1,16 @@
 package seedu.addressbook.commands;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class PrintCommandTest {
+
+    @Test
+    public void execute() {
+        PrintCommand command = new PrintCommand();
+        CommandResult result = command.execute();
+
+        assertEquals(PrintCommand.MESSAGE_PRINT, result.feedbackToUser);
+    }
+
 }
